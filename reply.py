080871e68f339979cd3849c2cf458cd1e25eee5f
Julie Eubank <julie@sensehealth.com>
2016-09-12 14:09:58 -0400

    happy emoji

--- reply.py
+++ reply.py
@@ -17,6 +17,8 @@ import logging
 from apscheduler.schedulers.background import BackgroundScheduler
 
 emojis = [u'\U0001F601',u'\U0001F602',u'\U0001F603',u'\U0001F604',u'\U0001F605',u'\U0001F606',u'\U0001F609',u'\U0001F60A',u'\U0001F60B',u'\U0001F60C',u'\U0001F60D',u'\U0001F60F',u'\U0001F612',u'\U0001F613',u'\U0001F614',u'\U0001F616',u'\U0001F618',u'\U0001F61A',u'\U0001F61C',u'\U0001F61D',u'\U0001F61E',u'\U0001F628',u'\U0001F629',u'\U0001F62A',u'\U0001F62B',u'\U0001F62D',u'\U0001F630',u'\U0001F631',u'\U0001F638',u'\U0001F639',u'\U0001F63A',u'\U0001F63B',u'\U0001F63C',u'\U0001F63D',u'\U0001F63F',u'\U0001F648',u'\U0001F649']
+happy_emojis = [u'\U0001F601',u'\U0001F602',u'\U0001F603',u'\U0001F604',u'\U0001F605',u'\U0001F606',u'\U0001F609',u'\U0001F60A',u'\U0001F60B',u'\U0001F60C',u'\U0001F60D',u'\U0001F63A',u'\U0001F63B',u'\U0001F63C',u'\U0001F63D',u'\U0001F63F',u'\U0001F648',u'\U0001F649']
+
 punc = ['.', '', '!', '!!', '!!!', '!!!', '-', '--']
 sorrys = ['Hey, I thought about what you said last night and I am so sorry!', "Wow, I couldn't sleep night. You were right!",
           "Hey again, I keep thinking about our conversation. I was very rude.", "Hi there, last night was really rough. You were right",
@@ -91,7 +93,7 @@ class ReplyToTweet(StreamListener):
                 num_lib_lines = 0
                 for l in lib_lines:
                     num_lib_lines += 1
-                lib_tweet = self.clean_tweet(lib_lines[randint(1, num_lib_lines - 1)].replace('\n', ''), screenName)
+                lib_tweet = self.clean_tweet(lib_lines[randint(1, num_lib_lines - 1)].replace('\n', '') + happy_emojis[randint(0, len(happy_emojis) - 1)] + happy_emojis[randint(0, len(happy_emojis) - 1)], screenName)
 
                 liberal_tweet(lib_tweet)
                 f.close()
