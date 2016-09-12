9bada3ae21eb8e55cb49b42e4e7598a06d5bee1b
Julie Eubank <julie@sensehealth.com>
2016-09-12 10:18:38 -0400

    error handling

--- run_it.py
+++ run_it.py
@@ -4,24 +4,41 @@ from lives_matter_hate_speech import lives_matter_bot, lives_matter_reply
 from uncle_hate_speech import tweet_bot, uncle_reply
 
 from threading import Thread
+import logging
+
+import sys
+import threading
+
+def except_me():
+    exc_type, exc_value = sys.exc_info()[:2]
+    err = 'Handling %s exception with message "%s" in %s' % (exc_type.__name__, exc_value, threading.current_thread().name)
+    logging.debug(err)
+
+logging.basicConfig(
+level=logging.DEBUG,
+filename='wtf.log',
+filemode='w')
 
 if __name__ == "__main__":
-    # Hate speech
-    m = Thread(target=mens_bot)
-    m.start()
-    g = Thread(target=gun_bot)
-    g.start()
-    l = Thread(target=lives_matter_bot)
-    l.start()
-    u = Thread(target=tweet_bot)
-    u.start()
+    try:
+        # Hate speech
+        m = Thread(target=mens_bot)
+        m.start()
+        g = Thread(target=gun_bot)
+        g.start()
+        l = Thread(target=lives_matter_bot)
+        l.start()
+        u = Thread(target=tweet_bot)
+        u.start()
 
-    # Reply bots
-    mr = Thread(target=mens_reply)
-    mr.start()
-    gr = Thread(target=gun_reply)
-    gr.start()
-    lr = Thread(target=lives_matter_reply)
-    lr.start()
-    ur = Thread(target=uncle_reply)
-    ur.start()
+        # Reply bots
+        mr = Thread(target=mens_reply)
+        mr.start()
+        gr = Thread(target=gun_reply)
+        gr.start()
+        lr = Thread(target=lives_matter_reply)
+        lr.start()
+        ur = Thread(target=uncle_reply)
+        ur.start()
+    except:
+        except_me()
